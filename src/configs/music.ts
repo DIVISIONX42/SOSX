ff57c83c969b51bee7c91d96250295e3eb3ce12b
DIVISION X42 <divisionx42@proton.me>
2026-01-05 18:00:14 +0100

    Update music configuration with new track
    Comment out the 'Sunflower' music entry and add 'BurningSystem.mp3'.

--- src/configs/music.ts
+++ src/configs/music.ts
@@ -1,10 +1,10 @@
 import type { MusicData } from "~/types";
 
 const music: MusicData = {
-  title: "Sunflower",
+/*  title: "Sunflower",
   artist: "Post Malone / Swae Lee",
   cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
-  audio: "music/sunflower.mp3",
+  audio: "music/sunflower.mp3",*/
 
   title: "BurningSystem.mp3",
   artist: "Mass / Suffers",

09d9db41d49d9cfc925be98c2200669487192ff2
DIVISION X42 <divisionx42@proton.me>
2026-01-05 17:58:26 +0100

    Add 'BurningSystem' music entry
    Added new music entry for 'BurningSystem' by Mass / Suffers.

--- src/configs/music.ts
+++ src/configs/music.ts
@@ -4,7 +4,12 @@ const music: MusicData = {
   title: "Sunflower",
   artist: "Post Malone / Swae Lee",
   cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
-  audio: "music/sunflower.mp3"
+  audio: "music/sunflower.mp3",
+
+  title: "BurningSystem.mp3",
+  artist: "Mass / Suffers",
+  cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
+  audio: "music/BurningSystem.mp3"
 };
 
 export default music;

39887c1ef9c8548d34913b89797b95214f4535ff
DIVISION X42 <divisionx42@proton.me>
2026-01-05 18:04:50 +0100

    Fix audio file extension for Burning System

--- src/configs/music.ts
+++ src/configs/music.ts
@@ -9,7 +9,7 @@ const music: MusicData = {
   title: "Burning🇩🇪System",
   artist: "Mass / Suffers",
   cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
-  audio: "music/Burningsystem.mp3"
+  audio: "music/Burningsystem.mp3.mp3"
 };
 
 export default music;
